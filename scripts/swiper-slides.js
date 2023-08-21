0f3b1b14e33d3232fa7a69ad841b6127f7f01932
Victor Yoshio Tanaka <yoshio.victor@hotmail.com>
2023-08-21 11:13:04 -0300

    first commit

---
+++ scripts/swiper-slides.js
@@ -0,0 +1,26 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  centeredSlides: false,
+  slidesPerGroupSkip: 1,
+  grabCursor: true,
+  keyboard: {
+    enabled: true,
+  },
+  breakpoints: {
+    769: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+    },
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
